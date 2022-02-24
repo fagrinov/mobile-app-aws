008de782e9c19a0885e0ab4cb90648193d3bdd2a
rghdslh3 <rghdslh3@gmail.com>
2022-02-24 12:27:32 +0200

    sign in security with user id

--- src/main/java/com/example/demo/MobileAppAwsApplication.java
+++ src/main/java/com/example/demo/MobileAppAwsApplication.java
@@ -21,4 +21,9 @@ public class MobileAppAwsApplication {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	public SpringApplicationContext springApplicationContext() {
+		return new SpringApplicationContext();
+	}
 }
